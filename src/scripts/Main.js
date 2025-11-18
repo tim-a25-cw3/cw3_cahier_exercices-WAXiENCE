7b0b37f30af97e95b6f03cc4fa43123cf49db60a
WAXiENCE <maxou.giard@gmail.com>
2025-11-18 12:58:01 -0500

    finit mais image qui bug???????????????

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,12 +1,28 @@
 import Icons from './utils/Icons.js';
-
+import Swiper from 'swiper/bundle';
+ 
+ 
 class Main {
   constructor() {
     this.init();
   }
-
+ 
   init() {
     Icons.load();
+ 
+    this.initSwiperReee();
+  }
+ 
+    initSwiperReee() {
+    const target = document.querySelector('.js-reee');
+    if (target) {
+      const swiper = new Swiper(target, {
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      });
+    }
   }
 }
 new Main();
+ 
